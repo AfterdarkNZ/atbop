4ecd93ff1ba905ad7e3a21297c717870ed1d03eb
Pete Mackay <greenworxnz@gmail.com>
2024-11-13 10:19:38 +1300

    updated logo address

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,7 @@ function Header() {
   return (
     <header>
       <div className="content">
-        <img
-          src="../../public/tree-icon-2.jpg"
-          alt="ATBOP Logo"
-          className="logo"
-        />
+        <img src="/tree-icon-2.jpg" alt="ATBOP Logo" className="logo" />
         <div className="header-text">
           <h1>Associated Therapists Bay of Plenty (ATBOP)</h1>
           <h3>Your pathway to personal and professional growth</h3>
